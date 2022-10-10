8fea8764ed30d57c8284d7339ff5a5d170578724
26harsh <harsh.gc@somaiya.edu>
2022-10-10 19:52:08 +0530

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,28 @@
+import 'package:firebase_core/firebase_core.dart';
+import 'package:flutter/material.dart';
+import 'package:quiz_tutorial_app/constants.dart';
+import 'package:quiz_tutorial_app/screens/home/home_screen.dart';
+
+Future main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp();
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        textTheme: Theme.of(context).textTheme.apply(bodyColor: kTextColor),
+        primarySwatch: Colors.blue,
+      ),
+      home: HomeScreen(),
+    );
+  }
+}
---
+++ lib/screens/details/components/body.dart
@@ -0,0 +1,77 @@
+import 'package:flutter/material.dart';
+import 'package:quiz_tutorial_app/constants.dart';
+import 'package:quiz_tutorial_app/models/Product.dart';
+
+import 'description.dart';
+import 'product_title_with_image.dart';
+
+class Body extends StatelessWidget {
+  final Product product;
+
+  const Body({Key? key, required this.product}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    //it is for total height and width
+    Size size = MediaQuery.of(context).size;
+    return SingleChildScrollView(
+      child: Column(
+        children: <Widget>[
+          SizedBox(
+            height: size.height,
+            child: Stack(
+              children: <Widget>[
+                Container(
+                    margin: EdgeInsets.only(top: size.height * 0.3),
+                    padding: EdgeInsets.only(
+                      top: size.height * 0.12,
+                      left: kDefaultPaddin,
+                      right: kDefaultPaddin,
+                    ),
+                    // height: 500,
+                    decoration: BoxDecoration(
+                        color: Colors.white,
+                        borderRadius: BorderRadius.only(
+                          topLeft: Radius.circular(24),
+                          topRight: Radius.circular(24),
+                        )),
+                    child: Column(
+                      children: <Widget>[
+                        Row(
+                          children: <Widget>[
+                            Column(
+                              children: <Widget>[
+                                Text("Description"),
+                                Description(product: product),
+                              ],
+                            )
+                          ],
+                        )
+                      ],
+                    )),
+                SizedBox(
+                    height: 50,
+                    child: OutlinedButton(
+                      onPressed: () {},
+                      child: Text("START QUIZ"),
+                    )),
+                ProductTitleWithImage(product: product),
+                SizedBox(
+                  width: kDefaultPaddin,
+                ),
+                Expanded(
+                    child: Image.asset(
+                  product.image,
+                  width: 250,
+                  height: 250,
+                  scale: 0.1,
+                  alignment: Alignment.bottomLeft,
+                ))
+              ],
+            ),
+          )
+        ],
+      ),
+    );
+  }
+}
